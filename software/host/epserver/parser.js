7f9c412d7eecb4eb8c2c8daa564f3c3808d5d552
Avishay Orpaz <avishorp@gmail.com>
2016-04-05 14:32:20 +0300

    Move the protocol parser into a module of its own

---
+++ software/host/epserver/parser.js
@@ -0,0 +1,71 @@
+'use strict'
+
+const stream = require('stream')
+const util = require('util')
+const SRBuffer = require('./shiftreg')
+
+const MESSAGE_LENGTH = 7
+const MSG_PROLOG = 0x85
+
+const MESSAGE_TYPE_WELCOME = 1
+const MESSAGE_TYPE_SET_COLOR = 2
+const MESSAGE_TYPE_INDICATE = 3
+const MESSAGE_TYPE_SYNC_RSP = 4
+
+const MESSAGE_OFFSET_TYPE = 1
+const MESSAGE_OFFSET_WELCOME_NUM = 2
+const MESSAGE_OFFSET_WELCOME_PERSONALITY = 3
+
+const PERSONALITY_HAT = 1
+const PERSONALITY_HAMMER = 2
+
+function ProtocolParser() {
+	stream.Writable.call(this)
+	this.msg = new SRBuffer(7)
+}
+
+util.inherits(ProtocolParser, stream.Writable)
+
+ProtocolParser.prototype._write = function(chunk, encoding, done) {
+	for(let b of chunk) {
+		this.msg.lshift(b)
+		
+		// Check the validity of the message
+		if (this.msg.buffer[0]==MSG_PROLOG) {
+			const checksum = this._checksum(this.msg.buffer)
+			if (this.msg.buffer[MESSAGE_LENGTH-1] != checksum)
+				this.emit('error', 'Incorrect checksum')
+			else 
+				this._doMessageParsing(this.msg.buffer)
+		}	
+	}
+}
+
+ProtocolParser.prototype._doMessageParsing = function(message) {
+	const type = message.readInt8(MESSAGE_OFFSET_TYPE)
+	if (type === MESSAGE_TYPE_WELCOME) {
+		const body = {
+			boardNum: message.readInt8(MESSAGE_OFFSET_WELCOME_NUM),
+			personality: message.readInt8(MESSAGE_OFFSET_WELCOME_PERSONALITY)==PERSONALITY_HAMMER?
+				'hammer' : 'hat'
+		}
+		this.emit('welcome', body)
+	}
+	else if (type === MESSAGE_TYPE_SYNC_RSP) {
+		// TODO
+	}
+	else
+		this.emit('error', util.format('Incorrect message type: %d', type))
+			
+}
+
+ProtocolParser.prototype._checksum = function(buf) {
+	let i
+	let checksum = 0
+	for(i=0; i < buf.length-1; i++)
+		checksum += buf[i]
+		
+	return checksum
+}
+
+module.exports = ProtocolParser
