6078dbecd616a09f5939bc7252072f72a8621f0d
Avishay Orpaz <avishorp@gmail.com>
2016-07-13 15:01:16 +0300

    Add TFTP server to the discovery module

--- software/host/discovery/index.js
+++ software/host/discovery/index.js
@@ -3,6 +3,8 @@
 const dgram = require('dgram')
 const util = require('util')
 const struct = require('python-struct')
+const tftp = require('tftp')
+
 
 // Discovery server
 //
@@ -40,13 +42,16 @@ module.exports = function(options, logger) {
 	// Initiate the UDP discovery socket
 	const srv = dgram.createSocket('udp4')
 	
+	
+	// Create UDP server
 	srv.on('error', err => {
 		logger.error(`Socket creation failed: ${err.stack}`)
 		srv.close();
 	})
 	
+	
 	srv.on('listening', () => {
-		logger.info("Listening")
+		logger.info("Discovery Server Listening")
 	})
 	
 	srv.on('message', (msg, rinfo) => {
@@ -64,6 +69,32 @@ module.exports = function(options, logger) {
 	})
 	
 	srv.bind(options.port)
+
+	// Create TFTP server
+	const tftpSrv = tftp.createServer({
+		root: options.firmware.directory,
+		port: options.firmware.port,
+		denyPUT: true
+	})
+	
+	tftpSrv.on('error', err => {
+		logger.error("TFTP Server failed initializing")
+		console.log(err)
+	})
+	tftpSrv.on('listening', _ => {
+		logger.info("TFTP Server listening")
+	})
+	tftpSrv.on('request', (req, res) => {
+		req.on('error', err => {
+			logger.error("[" + req.stats.remoteAddress + ":" + req.stats.remotePort +
+        		"] (" + req.file + ") " + err.message)
+		})
+		
+		logger.info(util.format("TFTP: Node %s requested file %s", req.stats.remoteAddress, req.file))
+	})
+	
+	tftpSrv.listen()
+
 }
 
 
